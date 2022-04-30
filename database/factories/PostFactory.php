b5dfc8e8d8589007febf9053d7a79bacfb08851d
joy2323 <joyjoel2011@gmail.com>
2022-04-30 23:17:19 +0200

    User, Post, Comment factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->title(),
+            'link' => $this->faker->url(),
+        ];
+    }
+}
